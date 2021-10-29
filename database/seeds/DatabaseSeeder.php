4648ed8fcfe77e34f18e639fc44167900f5ac16f
JosueGarrido <josue.garrido@epn.edu.ec>
2021-10-29 01:50:06 -0500

    procfile

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,23 +14,23 @@ class DatabaseSeeder extends Seeder
     {
         Schema::disableForeignKeyConstraints();
 
-        $this->call(UsersTableSeeder::class);
-        $this->call(CulturalProyectsTableSeeder::class);
-        $this->call(RecognitionsTableSeeder::class);
-        $this->call(SalesTableSeeder::class);
-        $this->call(WebLinksTableSeeder::class);
-        $this->call(TrayectoryTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        //$this->call(CulturalProyectsTableSeeder::class);
+        //$this->call(RecognitionsTableSeeder::class);
+        //$this->call(SalesTableSeeder::class);
+        //$this->call(WebLinksTableSeeder::class);
+        //$this->call(TrayectoryTableSeeder::class);
         $this->call(Category1TableSeeder::class);
         $this->call(Category2TableSeeder::class);
         $this->call(Category3TableSeeder::class);
         $this->call(Category4TableSeeder::class);
-        $this->call(ProductTableSeeder::class);
-        $this->call(QuestionsTableSeeder::class);
-        $this->call(AnswersTableSeeder::class);
-        $this->call(ReputationTableSeeder::class);
-        $this->call(Audio_Video_FormatsTableSeeder::class);
-        $this->call(Photo_FormatsTableSeeder::class);
-        $this->call(FollowersTableSeeder::class);
+        //$this->call(ProductTableSeeder::class);
+        //$this->call(QuestionsTableSeeder::class);
+        //$this->call(AnswersTableSeeder::class);
+        //$this->call(ReputationTableSeeder::class);
+        //$this->call(Audio_Video_FormatsTableSeeder::class);
+        //$this->call(Photo_FormatsTableSeeder::class);
+        //$this->call(FollowersTableSeeder::class);
 
         Schema::enableForeignKeyConstraints();
     }
